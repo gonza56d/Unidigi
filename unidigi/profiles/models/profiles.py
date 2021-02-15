cbec48d452cad18d865b14e288db685fa9da0af8
Gonza Garcia <gonzalo.garcia01@globant.com>
2021-02-15 05:04:08 -0300

    User manager

--- unidigi/profiles/models/profiles.py
+++ unidigi/profiles/models/profiles.py
@@ -23,5 +23,5 @@ class Profile(BaseModel):
     first_name = models.CharField(max_length=100, validators=[CommonRegex.LETTERS])
     last_name = models.CharField(max_length=100, validators=[CommonRegex.LETTERS])
     birthday = models.DateField(null=True, blank=True)
-    picture = models.ImageField(upload_to='profiles')
+    picture = models.ImageField(upload_to='profiles', blank=True)
     bio = models.CharField(max_length=3000, null=True, blank=True)
--- unidigi/users/models.py
+++ unidigi/users/models.py
@@ -3,21 +3,47 @@ from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
 
 # Project
+from unidigi.profiles.models import Profile
 from unidigi.utils.models import BaseModel, CommonRegex
 
 
-class UserManager(UserManager):
+class UnidigiUserManager(UserManager):
 
-    def _create_user(self, username, email, password, **extra_fields):
+    def _create_user(self, username, email, password, first_name, last_name, **extra_fields):
         """
         Ensure username and email are in lowercase before calling the framework
         implementation of method.
         """
+        if not first_name or not last_name:
+            raise ValueError('First name and last name are required.')
         username = username or ''
         username = username.lower()
         email = email or ''
         email = email.lower()
-        return super()._create_user(self, username, email, password, **extra_fields)
+        user = super()._create_user(self, username, email, password, **extra_fields)
+        profile = Profile()
+        profile.user = user
+        profile.first_name = first_name
+        profile.last_name = last_name
+        return user
+
+    def create_user(self, username, email=None, password=None, first_name=None,
+                    last_name=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(username, email, password, first_name, last_name, **extra_fields)
+
+    def create_superuser(self, username, email=None, password=None, 
+                         first_name=None, last_name=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(username, email, password, first_name, last_name, **extra_fields)
 
 
 class User(BaseModel, AbstractUser):
@@ -38,7 +64,7 @@ class User(BaseModel, AbstractUser):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
 
-    objects = UserManager()
+    objects = UnidigiUserManager()
 
     def __str__(self):
         return self.username
